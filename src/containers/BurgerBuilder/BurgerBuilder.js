8849cce6d97b253fd20fbbb5318fe123811fd976
Pranav Kudva <pranavkv@thoughtworks.com>
2021-03-07 15:27:15 +0530

    show modal only when "order now" is clicked

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,7 +26,8 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false,
     }
 
     updatePurchaseState(ingredients) {
@@ -71,6 +72,10 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updatedIngredients);
     }
 
+    purchaseHandler =  () => {
+        this.setState({purchasing:true});
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -81,7 +86,7 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
 
@@ -91,6 +96,7 @@ class BurgerBuilder extends Component {
                     ingredientsRemoved={this.removeIngredientHandler} 
                     disabled={disabledInfo} 
                     purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler}
                     price={this.state.totalPrice} />
             </Aux>
         );

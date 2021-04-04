5936bfb048c2f72cfa7f1f4524cc24df863b4cf2
Pranav Kudva <pranavkv@thoughtworks.com>
2021-04-04 20:55:31 +0530

    save order to firebase when on clicking Continue on order modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -81,7 +82,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Pranav Kudva',
+                address: {
+                    street: 'Blahblah street', 
+                    zipCode: '123456', 
+                    country: 'India', 
+                },
+                email: 'dummy@mail.com'
+            },
+            deliveryMethod: 'zipzapzoop',
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {

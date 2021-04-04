19818b0c48663df746a9c3ac13a1bd80ed91f990
Pranav Kudva <pranavkv@thoughtworks.com>
2021-04-04 23:42:01 +0530

    add request / response interceptors & eject on unmount

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -11,18 +11,23 @@ const withErrorHandler = (WrappedComponent, axios) => {
         }
 
         componentWillMount () {
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             });
 
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             });
         }
 
         errorConfirmedHandler = () => {
             this.setState({error: null});
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.request.eject(this.resInterceptor);
         }
 
         render() {

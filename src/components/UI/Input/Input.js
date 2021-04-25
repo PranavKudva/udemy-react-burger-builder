977470bc467dd748885a8861e9d37a7a6136ffe1
Pranav Kudva <pranavkv@thoughtworks.com>
2021-04-25 18:48:37 +0530

    correction for delivery option select in input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,56 +1,59 @@
-import React from "react";
-import classes from "./Input.module.css";
+import React from 'react';
 
-const input = (props) => {
-  let inputElement = null;
-  const inputClasses = [classes.InputElement];
+import classes from './Input.module.css';
 
-  if (props.invalid && props.shouldValidate && props.touched) {
-    inputClasses.push(classes.Invalid);
-  }
+const input = ( props ) => {
+    let inputElement = null;
+    const inputClasses = [classes.InputElement];
 
-  switch (props.elementType) {
-    case "input":
-      inputElement = <input 
-          className={inputClasses.join(' ')} 
-          {...props.elementConfig} 
-          value={props.value} 
-          onChange={props.changed}/>;
-      break;
-    case "textArea":
-      inputElement = <textarea 
-          className={classes.InputElement} 
-          {...props.elementConfig} 
-          value={props.value} 
-          onChange={props.changed}/>;
-      break;
-    case "select":
-      inputElement = (
-        <select 
-          className={classes.InputElement}  
-          value={props.value}>
-          {
-            props.elementConfig.options.map(option => (
-              <option key={option.value} value={option.value}>
-                {option.displayValue}
-              </option>
-            ))}
-        </select>
-      );
-      break;
-    default:
-      inputElement = <input 
-          className={classes.InputElement} 
-          {...props.elementConfig}
-          value={props.value}/>;
-  }
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+    }
 
-  return (
-    <div className={classes.Input}>
-      <label className={classes.Label}>{props.label}</label>
-      {inputElement}
-    </div>
-  );
+    switch ( props.elementType ) {
+        case ( 'input' ):
+            inputElement = <input
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+            break;
+        case ( 'textarea' ):
+            inputElement = <textarea
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+            break;
+        case ( 'select' ):
+            inputElement = (
+                <select
+                    className={inputClasses.join(' ')}
+                    value={props.value}
+                    onChange={props.changed}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
+            break;
+        default:
+            inputElement = <input
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+    }
+
+    return (
+        <div className={classes.Input}>
+            <label className={classes.Label}>{props.label}</label>
+            {inputElement}
+        </div>
+    );
+
 };
 
 export default input;

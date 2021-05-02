311245e5f81dbfcd53639eb0edd9e21d26d51026
Pranav Kudva <pranavkv@thoughtworks.com>
2021-05-02 19:24:53 +0530

    redirect to burgerbuilder post purchase successful
    ...also fix auto redirect to burger builder post purchased successful

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import Spinner from '../../components/UI/Spinner/Spinner';
-import * as burgerBuilderActions from "../../store/actions/index";
+import * as actions from "../../store/actions/index";
 
 class BurgerBuilder extends Component {
     // constructor() {
@@ -46,6 +46,7 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
+        this.props.onInitPurchase();
         this.props.history.push('/checkout');
     }
 
@@ -105,9 +106,10 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
-        onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
-        onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients()),
+        onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
+        onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
+        onInitIngredients: () => dispatch(actions.initIngredients()),
+        onInitPurchase: () => dispatch(actions.purchaseInit())
     }
 }
 
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -3,6 +3,7 @@ import * as actionTypes from "../actions/actionTypes";
 const initialState = {
     orders: [],
     loading: false,
+    purchased: false,
 };
 
 const reducer = (state = initialState, action) => {
@@ -15,14 +16,24 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 loading: false,
+                purchased: true,
                 orders: state.orders.concat(newOrder),
             };
-        case actionTypes.PURCHASE_BURGER_FAIL:
         case actionTypes.PURCHASE_BURGER_START:
             return {
                 ...state,
                 loading: true,
             };
+        case actionTypes.PURCHASE_INIT:
+            return {
+                ...state,
+                purchased: false,
+            }
+        case actionTypes.PURCHASE_BURGER_FAIL:
+            return {
+                ...state,
+                loading: false,
+            }
         default:
             return state;
     }

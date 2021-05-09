eef703e243d0f317063ee652a19c2f8ce7a389b6
Pranav Kudva <pranavkv@thoughtworks.com>
2021-05-09 19:55:26 +0530

    skip authentication if token still valid on reload of entire app

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from "./containers/Checkout/Checkout";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import * as actions from "./store/actions/index";
+import { connect } from 'react-redux';
 
 class App extends Component {
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ show: false });
-    }, 5000);
+    this.props.onTryAutoSignup();
   }
 
   render() {
@@ -32,4 +32,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState()),
+  };
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(App));

81089d00fbc88a0d9ab1bd62b34c3c25d7fe1956
Pranav Kudva <pranavkv@thoughtworks.com>
2021-05-15 20:28:16 +0530

    add find test for child components to navigation items

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -23,4 +23,8 @@ describe('<NavigationItems />', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 
+    it('should an exact logout button', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link='/logout'>Logout</NavigationItem>)).toEqual(true);
+    });
 });
